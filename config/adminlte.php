78eba02aea3fca7e1a1109e3332af929928ad46d
Raidel <rraidel85@gmail.com>
2022-01-27 09:11:59 -0800

    Remove asset helper from adminlte config file

--- config/adminlte.php
+++ config/adminlte.php
@@ -47,7 +47,7 @@ return [
     */
 
     'logo' => 'Gestion<b>Calidad</b>',
-    'logo_img' => asset('images/c-image3.jpg'),
+    'logo_img' => '/images/c-image3.jpg',
     'logo_img_class' => 'brand-image img-circle',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
