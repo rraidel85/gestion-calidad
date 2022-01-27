84335ed22b7b9d12afb4c7b7936e90c3c13b1903
Raidel <rraidel85@gmail.com>
2022-01-26 22:46:30 -0800

    Set livewire and login,register dark theme in adminlte config

--- config/adminlte.php
+++ config/adminlte.php
@@ -102,12 +102,12 @@ return [
     |
     */
 
-    'classes_auth_card' => 'card-outline card-primary',
+    'classes_auth_card' => 'bg-gradient-dark',
     'classes_auth_header' => '',
-    'classes_auth_body' => '',
-    'classes_auth_footer' => '',
-    'classes_auth_icon' => '',
-    'classes_auth_btn' => 'btn-flat btn-primary',
+    'classes_auth_body' => 'bg-gradient-dark',
+    'classes_auth_footer' => 'text-center',
+    'classes_auth_icon' => 'fa-fw text-light',
+    'classes_auth_btn' => 'btn-flat btn-light',
 
     /*
     |--------------------------------------------------------------------------
@@ -472,5 +472,5 @@ return [
     |
     */
 
-    'livewire' => false,
+    'livewire' => true,
 ];
